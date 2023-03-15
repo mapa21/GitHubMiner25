14ebb678581ffc347ea3f3a029415ce21eb35fba
DovydasVad <vadisius.dovydas@gmail.com>
2023-03-15 19:47:55 +0100

    Added Commit and File classes

--- src/main/java/softwaredesign/extraction/Commit.java
+++ src/main/java/softwaredesign/extraction/Commit.java
@@ -1,4 +1,16 @@
 package softwaredesign.extraction;
 
+import java.util.Date;
+import java.util.List;
+
 public class Commit {
+    public String authorName;
+    public String authorEmail;
+    public Date date;
+    public String description;
+    public List<File> files;
+    public String hash;
+    public Integer additions;
+    public Integer deletions;
+    public Boolean isMerge;
 }
---
+++ src/main/java/softwaredesign/extraction/File.java
@@ -0,0 +1,7 @@
+package softwaredesign.extraction;
+
+public class File {
+    public String name;
+    public Integer sizeBeforeCommit;
+    public Integer sizeAfterCommit;
+}
