13ccb7afe484944f310573b8440abb3e9a6829dd
mapa21 <63611280+mapa21@users.noreply.github.com>
2023-03-15 18:40:37 +0100

    Added: structure Account and Repository classes

---
+++ src/main/java/softwaredesign/Account.java
@@ -0,0 +1,10 @@
+package softwaredesign;
+
+public class Account {
+    public String name;
+    private String password;
+    public String token;
+    public Boolean login(String password){
+        return true;
+    }
+}
