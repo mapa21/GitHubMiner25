a4bd3c6009bd6372235abdf4664a88550d14f15d
DovydasVad <vadisius.dovydas@gmail.com>
2023-03-16 13:50:17 +0100

    Added NumberOfLines and TopCollaborators metrics

--- src/main/java/softwaredesign/extraction/MultipleData.java
+++ src/main/java/softwaredesign/extraction/MultipleData.java
@@ -2,14 +2,21 @@ package softwaredesign.extraction;
 
 import softwaredesign.utilities.NameValue;
 
+import java.util.List;
+
 public abstract class MultipleData extends Metric{
-    protected NameValue<Integer>[] data;
-//    protected MultipleData(NameValue<Integer>[] data) { //not sure if this is needed
+    protected List<NameValue<Integer>> data;
+//    protected MultipleData(List<NameValue<Integer>> data) { //not sure if this is needed
 //        this.data = data;
 //    }
     @Override
     protected String contentToString() {
         //convert data array to string
         return null;
+    }
+
+    // TODO: remove after testing
+    public List<NameValue<Integer>> getValue() {
+        return data;
     }
 }
--- src/main/java/softwaredesign/extraction/metrics/TopCollaborators.java
+++ src/main/java/softwaredesign/extraction/metrics/TopCollaborators.java
@@ -1,15 +1,40 @@
 package softwaredesign.extraction.metrics;
 
 import softwaredesign.extraction.Commit;
-import softwaredesign.extraction.SingleData;
+import softwaredesign.extraction.MultipleData;
+import softwaredesign.utilities.NameValue;
 
-public class TopCollaborators extends SingleData {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.Math.min;
+
+public class TopCollaborators extends MultipleData {
     protected String name = "Top Collaborators";
-    protected String description = "Sample Description";
+    protected String description = "Returns the list of Top 10 collaborators sorted by the number of commits in decreasing order";
 
-    protected TopCollaborators(Commit[] commits) {
-        int value = 0;
-        //implementation
-        this.value = value;
+    public TopCollaborators(List<Commit> commits) {
+        List<NameValue<Integer>> data = new ArrayList<>();
+        Map<String, Integer> collaboratorCommits = new HashMap<>();
+
+        for (Commit commit : commits) {
+            Integer currentCommits = collaboratorCommits.get(commit.authorName);
+            if (currentCommits == null) {
+                currentCommits = 0;
+            }
+            collaboratorCommits.put(commit.authorName, currentCommits + 1);
+        }
+
+        int collaboratorsToAdd = min(10, collaboratorCommits.size());
+
+        for (var entry : collaboratorCommits.entrySet()) {
+            data.add(new NameValue<>(entry.getKey(), entry.getValue()));
+            if (data.size() == collaboratorsToAdd) {
+                break;
+            }
+        }
+        this.data = data;
     }
 }
