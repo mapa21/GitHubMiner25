693aa3e6480a36e514af0f336a3cb3b0daed25d2
mapa21 <63611280+mapa21@users.noreply.github.com>
2023-03-17 18:57:07 +0100

    Added: create res folder for clone()

--- src/main/java/softwaredesign/Repository.java
+++ src/main/java/softwaredesign/Repository.java
@@ -1,12 +1,18 @@
 package softwaredesign;
 
+import softwaredesign.extraction.Commit;
+import softwaredesign.extraction.Metric;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Repository {
     public String name;
@@ -15,34 +21,44 @@ public class Repository {
     private Date lastUpdated;
     private String path;
     private String metricsListHash;
+    private List<Metric> metrics;
+
+    public Repository(String name, String owner, String token){
+        this.name = name;
+        this.owner = owner;
+        this.token = token;
+    }
 
     public void enter(){}
     protected Boolean clone(){
         Process process;
-        String[] commands = {"git clone https://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@github.com/ComputerScienceEducation/co-lab.git", "git log"};
-        String[] locations = {"res/", "res/co-lab/"};
-        for (int i = 0; i < commands.length; i++) {
-            System.out.println(commands[i] + " at " + locations[i]);
-            try {
-                process = Runtime.getRuntime().exec(commands[i], null, new File(locations[i]));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            List<String> output = null;
-            try {
-                output = getOutput(process);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println(output);
+        //create res folder
+        File file = new File("res");
+        if (file.mkdir()) {
+            System.out.println("dir created successfully");
+        } else {
+            System.out.println("Unsuccessful dir creation");
         }
+
+        String url = "https://" + this.token + "@github.com/" + this.owner + "/" + this.name + ".git";
+        url = "https://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@github.com/ComputerScienceEducation/co-lab.git";    //DELETE
+
+        String[] commands = {"git clone " + url, "git log --stat"};
+        String[] locations = {"res/", "res/" + this.name};
+
+        for (int i = 0; i < commands.length; i++){
+            process = Runtime.getRuntime().exec(commands[i], null, new File(locations[i]));
+            List<String> output = getOutput(process);
+            List<Commit> commits = parseLog(output);
+        }
+
         return true;
     }
 
     public static List<String> getOutput(Process process) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        List<String> lines = new ArrayList<String>();
-        String line = "";
+        List<String> lines = new ArrayList<>();
+        String line;
         while ((line = reader.readLine()) != null) {
             lines.add(line);
         }
